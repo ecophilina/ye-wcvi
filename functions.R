c224bd237f23d1a6494eaadd281b60c3773d4927
Sean Anderson <sean@seananderson.ca>
2021-05-27 10:23:09 -0700

    Make map plotting function

--- functions.R
+++ functions.R
@@ -47,93 +47,201 @@ load_boundaries <- function(utm_zone) {
 get_diag <- function(m, response = "catch_count", variable = "depth_scaled", colour_var = "depth_m", start_year = 2007) {
   predictions <- predict(m)
   predictions$residuals <- residuals(m)
-  
+
   predictions <- predictions %>% filter (year >= start_year)
   predictions$est_exp <- exp(predictions$est)
-  
+
   print("R^2:")
   r2 <- cor(predictions$est, predictions[[response]])^2
   print(r2)
-  
+
   print("")
   print("AIC:")
   print(AIC(m))
-  
+
   print("")
   print("MSE:")
   print(mean(predictions$residuals^2))
-  
+
   plot_map <- function(dat, column = "est") {
     ggplot(dat, aes_string("X", "Y", colour = column)) +
       geom_point(alpha = 0.5) +
       coord_fixed()+
-      gfplot::theme_pbs() 
+      gfplot::theme_pbs()
   }
-  
+
   g <- plot_map(predictions, "est") +
     scale_colour_viridis_c() +
     ggtitle("Prediction (fixed effects + all random effects)")
   print(g)
-  
+
   # g <- plot_map(predictions, "est_non_rf") +
   #   ggtitle("Prediction (fixed effects only)") +
   #   scale_colour_viridis_c()
   # print(g)
-  # 
+  #
   #   g <- plot_map(predictions, "est_rf") +
   #     ggtitle("All random effects only") +
   #     scale_colour_gradient2()
   #   print(g)
-  
+
   g <- plot_map(predictions, "omega_s") +
     ggtitle("Spatial random effects only") +
     scale_colour_gradient2()
   print(g)
-  
+
   g <- plot_map(predictions, "epsilon_st") +
     ggtitle("Spatiotemporal random effects only") +
     facet_wrap(~year) +
     scale_colour_gradient2()
   print(g)
-  
+
   g <- plot_map(predictions, "residuals") +
     ggtitle("Residuals") +
     facet_wrap(~year) +
     scale_colour_gradient2()
   print(g)
-  
+
   g <- ggplot(predictions, aes_string("est_exp", response)) +
     geom_point(alpha = 0.2) +
     facet_wrap(~year) +
     coord_fixed() +
     geom_abline()+
-    gfplot::theme_pbs() 
+    gfplot::theme_pbs()
   print(g)
-  
+
   g <- ggplot(predictions, aes(est, residuals)) +
     geom_point(alpha = 0.2) +
     geom_smooth()+
-    gfplot::theme_pbs() 
+    gfplot::theme_pbs()
   print(g)
-  
+
   g <- ggplot(predictions, aes_string(variable, "residuals", colour = colour_var)) +
     geom_point(alpha = 0.4, size = 1.2) +
     geom_smooth(colour="grey", size = 1.2) +
-    scale_colour_viridis_c(option = "B", direction = -1, begin = 0, end = 0.7, 
+    scale_colour_viridis_c(option = "B", direction = -1, begin = 0, end = 0.7,
       limits= c(min(predictions[colour_var]), max(predictions[colour_var]))) + #, trans= sqrt
     # facet_wrap(~year, scales = "free_x")+
     gfplot::theme_pbs() + theme(
       axis.text.y = element_text(size = 14),
       axis.text.x = element_blank(), axis.ticks.x = element_blank())
   print(g)
-  
+
   g <- ggplot(predictions, aes_string(variable, "residuals", colour = colour_var)) +
     geom_point(alpha = 0.4, size = 0.7) +
     geom_smooth(colour="grey", size = 0.7) +
-    scale_colour_viridis_c(option = "B", direction = -1, begin = 0, end = 0.7, 
+    scale_colour_viridis_c(option = "B", direction = -1, begin = 0, end = 0.7,
       limits= c(min(predictions[colour_var]), max(predictions[colour_var]))) + #, trans= sqrt
     facet_wrap(~year, scales = "free_x")+
     gfplot::theme_pbs() + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
   print(g)
-  
+
+}
+
+map_predictions <- function(
+  pred_data,
+  obs_data,
+  size_aes = (catch_count / hook_count) * 100,
+  title = "",
+  size = "Observed fish\nper 100 hooks",
+  fill = "Predicted fish\nper 100 hooks") {
+  utm_zone9 <- 3156
+  # download from:
+  # https://www.ngdc.noaa.gov/mgg/shorelines/data/gshhg/latest/
+  if (!file.exists("data-generated/coast_gshhg.rds")) {
+    coast_gshhg <- sf::read_sf("data/gshhg-shp-2.3.7/GSHHS_shp/f/GSHHS_f_L1.shp") %>%
+      st_crop(c(xmin = -133, ymin = 47, xmax = -121, ymax = 55))
+    coast_gshhg_proj <- sf::st_transform(coast_gshhg, crs = utm_zone9)
+    saveRDS(coast_gshhg_proj, file = "data-generated/coast_gshhg.rds")
+  } else {
+    coast_gshhg_proj <- readRDS("data-generated/coast_gshhg.rds")
+  }
+  # ggplot(coast_gshhg) + geom_sf()
+
+  focal_area <- sf::st_read(dsn = "shape-files/taaqwiihak_areaVer2.shp",
+    layer = "taaqwiihak_areaVer2", quiet = TRUE)
+  focal_area_proj <- sf::st_transform(focal_area, crs = utm_zone9)
+
+  library(PBSmapping) # needs this for some reason
+  majorbound <- load_boundaries(9)
+  # attributes(majorbound)$PolyData
+  # 5AB
+  bound5Bnorth <- fortify(majorbound) %>%
+    filter(PID %in% c(6)) %>%
+    filter( # POS != 46 & # strange jog over HG
+      X > 200 & X < 560 & Y > 5700
+    )
+  bound5Anorth <- fortify(majorbound) %>%
+    filter(PID %in% c(5)) %>%
+    filter(
+      X > 200 & X < 560 & Y > 5650
+    )
+  # 3CD
+  bound3Cnorth <- fortify(majorbound) %>%
+    filter(PID %in% c(3)) %>%
+    filter(X > 200 & X < 725 & Y > 5400) #%>%
+  # gfplot:::utm2ll(utm_zone = 9)
+  bound3Dnorth <- fortify(majorbound) %>%
+    filter(PID %in% c(4)) %>%
+    filter(X > 200 & X < 600 & Y > 5550)
+
+  g <- ggplot(focal_area_proj) +
+    geom_tile(
+      data = filter(pred_data, year == 2020),
+      aes(X * 100000, Y * 100000, fill = exp(est)),
+      width = 2000, height = 2000, colour = NA
+    ) +
+    geom_line( # add major management region boundaries
+      data = bound3Cnorth,
+      aes(X * 1e3, Y * 1e3), colour = "grey30", lty = 1,
+      inherit.aes = F
+    ) +
+    geom_line( # add major management region boundaries
+      data = bound3Dnorth,
+      aes(X * 1e3, Y * 1e3), colour = "grey30", lty = 1,
+      inherit.aes = F
+    ) +
+    geom_line( # add major management region boundaries
+      data = bound5Anorth,
+      aes(X * 1e3, Y * 1e3), colour = "grey30", lty = 1,
+      inherit.aes = F
+    ) +
+    geom_sf(data = coast_gshhg, size = 0.1, fill = "grey75", col = "grey75") +
+    scale_fill_viridis_c(
+      # trans = ggsidekick::fourth_root_power_trans(),
+      trans = "sqrt",
+      option = "D"
+    ) +
+    labs(fill = fill, size = size) +
+    geom_point(
+      data = obs_data,
+      mapping = aes(
+        X * 1e5, Y * 1e5,
+        size = {{size_aes}}
+      ), pch = 21,
+      inherit.aes = FALSE, colour = "grey10", alpha = 0.7
+    ) +
+    scale_size_area(max_size = 3) +
+    geom_sf(colour = "red", fill = NA, size = 0.30) + # add focal area
+    annotate("text", x = -128.8, y = 51.1, label = "5A") +
+    annotate("text", x = -128.8, y = 50.3, label = "3D") +
+    annotate("text", x = -128.8, y = 48.8, label = "3C")
+  if (title != "") {
+    g <- g + ggtitle(title)
+  }
+  g <- g +
+    theme(panel.grid.major = element_line(colour = "grey89", size = 0.3)) +
+    coord_sf(
+      xlim = c(230957.7 + 200000, 1157991 - 300000),
+      ylim = c(5366427, 6353456 - 500000)
+    ) +
+    guides(
+      fill = guide_colorbar(order = 1),
+      size = guide_legend(order = 0)
+    ) +
+    theme(axis.title = element_blank(),
+      legend.position = c(1, 1),
+      legend.justification = c(1, 1)
+    )
+  g
 }
