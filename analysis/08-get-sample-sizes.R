d044297fd048075ca5ad0ef06c24fb609eb68675
ecophilina <paenglis@lakeheadu.ca>
2022-12-20 13:32:50 -0800

    check that ye sample sizes are the same

--- analysis/08-get-sample-sizes.R
+++ analysis/08-get-sample-sizes.R
@@ -11,6 +11,9 @@ library(ggsidekick)
 hal_model <- "w-deeper-500kn-delta-AR1-aniso-dec22"
 m_hal <- readRDS(paste0("models/halibut-model-", hal_model, "-tmbfit.rds"))
 
+# ye_model <- "w-deeper-all-yrs-500kn-delta-iid-aniso-dec22"
+# m_ye <- readRDS(paste0("models/yelloweye-model-", ye_model, "-tmbfit.rds"))
+
 focal_area <- sf::st_read(
   dsn = "shape-files/taaqwiihak_areaVer2.shp",
   layer = "taaqwiihak_areaVer2", quiet = TRUE
@@ -18,6 +21,7 @@ focal_area <- sf::st_read(
 focal_area_proj <- sf::st_transform(focal_area, crs = 3156)
 
 m_dat <- m_hal$data %>% mutate(X2 = X * 100000, Y2 = Y * 100000)
+# m_dat <- m_ye$data %>% mutate(X2 = X * 100000, Y2 = Y * 100000)
 
 m_dat_sf <- st_as_sf(m_dat, coords = c("X2", "Y2"), crs = 3156)
 keep <- st_intersects(focal_area_proj, m_dat_sf)
